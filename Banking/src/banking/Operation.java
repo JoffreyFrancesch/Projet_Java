744ac653b0f0adb4edd129ea99fb20d3e9ddfa0f
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-11-10 17:51:27 +0100

    Add Create Account

--- Banking/src/banking/Operation.java
+++ Banking/src/banking/Operation.java
@@ -5,39 +5,41 @@ import java.util.Scanner;
 
 public class Operation{
   Scanner scanner = new Scanner(System.in);
-  //Banque banque = new Banque();
-  public void creer_compte(){
-    String prenom, nom, age = null;
-    double premier_depot = 0;
+  Banque banque = new Banque();
 
+
+  public void creer_compte(){
+    String prenom, nom;
+    int age;
+    double premier_depot = 0;
     boolean flag = false;
 
-    System.out.print("Entrer votre Prénom : ");
+    System.out.print("Entrer votre Prénom : ");//Recuperation du prenom
     prenom = scanner.nextLine();
-    System.out.print("Entrer votre Nom : ");
+
+    System.out.print("Entrer votre Nom : ");//Recuperation du nom
     nom = scanner.nextLine();
-    System.out.print("Entrer votre Age : ");
-    age = scanner.nextLine();
+
+    System.out.print("Entrer votre Age : ");//Recuperation de l'age
+    age = scanner.nextInt();
 
     do {
-      System.out.print("Entrer un premier dépot d'argent (>0) : ");
+      System.out.print("Entrer un premier dépot d'argent (>0) : ");//Recuperation du premier depot
       premier_depot = scanner.nextDouble();
 
-      if(premier_depot < 0){
+      if(premier_depot < 0){//Verif depot > 0
         System.out.println("Vous devez mettre un montant positif");
       } else {
         flag = true;
       }
     }while(!flag);
 
-  //banque.ajout_client(prenom,nom,age,premier_depot);
-  System.out.println("Vous avez ouvert un comptre au nom de "+ nom + prenom + " %s avec un solde initial de " + premier_depot + "\n");
-
+    Client client = new Client(prenom,nom,age,premier_depot);//Création du nouveau client
+    banque.ajout_client(prenom,nom,age,premier_depot);//Ajout du Client a la liste des clients
   }
 
   public void faire_depot(){
-    //int compte = selection_compte();
-
+    //Todo
   }
 
   public void faire_retrait(){
@@ -46,6 +48,7 @@ public class Operation{
 
   public void historique(){
     //Todo
+    //client.info();
   }
 
   public void demande_pret(){
