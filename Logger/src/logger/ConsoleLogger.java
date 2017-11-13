c3c7362aeeaadb310fff3b91c64daf258c6a3749
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-11-13 14:56:04 +0100

    Change on Logger

--- Logger/src/logger/ConsoleLogger.java
+++ Logger/src/logger/ConsoleLogger.java
@@ -1,9 +1,14 @@
 package logger;
 
 public class ConsoleLogger implements Logger {
-
-	public void log(String message) {
-		System.out.println(message);
+	public void debug(String category, String message){
+		System.out.println(category + " " + message);
+	}
+	public void info(String category, String message){
+		System.out.println(category + " " + message);
+	}
+	public void error(String category, String message){
+		System.out.println(category + " " + message);
 	}
 
 }
--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-#javac -d Logger/bin $(find Logger/src -name *.java)
+javac -d Logger/bin $(find Logger/src -name *.java)
 #javac -d Testframework/bin $(find Testframework/src -name *.java)
 javac -d Banking/bin $(find Banking/src -name *.java)
