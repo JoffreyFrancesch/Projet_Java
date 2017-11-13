7dc20e5993bc35c70a8088639328361f9da60d84
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-11-13 14:52:13 +0100

    Modifications des scripts de compilation

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
+#javac -d Logger/bin $(find Logger/src -name *.java)
+#javac -d Testframework/bin $(find Testframework/src -name *.java)
 javac -d Banking/bin $(find Banking/src -name *.java)
-java -classpath Banking/bin banking.Main
---
+++ launch_banking.sh
@@ -0,0 +1,1 @@
+java -classpath Banking/bin banking.Main
