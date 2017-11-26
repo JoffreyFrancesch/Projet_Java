8c38636cb5adba1e2162c0faf6c97d4fda9a9121
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-11-26 11:51:10 +0100

    Integration de la compilatio de TestFrameWork

--- compile.sh
+++ compile.sh
@@ -7,3 +7,5 @@ javac -d Logger/bin $(find Logger/src -name *.java)
 rm -rf Banking/bin
 mkdir Banking/bin
 javac -classpath Logger/bin -d Banking/bin $(find Banking/src -name *.java)
+
+javac -classpath Banking/bin -d Testframework/bin $(find Testframework/src -name *.java)
