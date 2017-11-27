fc776d4896c2b87d64a2ae689ccf62e326b8e42e
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-11-27 08:47:41 +0100

    fichier de compilation des test

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-java -classpath Banking/bin:Testframework/bin testframework.maintest
+#java -classpath Banking/bin:Testframework/bin testframework.maintest
+java -classpath Banking/bin:Logger/bin:Testframework/bin testframework.RunTest logger.LoggerFactory logger.ContextualLogger logger.CompositeLogger logger.FileLogger logger.Logger logger.ConsoleLogger banking.Client banking.Affichage banking.Banque banking.Credit banking.Main banking.Menu banking.Operation
